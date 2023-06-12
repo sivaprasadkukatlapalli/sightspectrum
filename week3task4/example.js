629577681eae873b3a4e011ebc3f62620db096e3
unknown <sivaprasadkukatlapalli>
2023-06-12 09:58:03 +0530

    first commit

---
+++ week3task4/example.js
@@ -0,0 +1,9 @@
+const message=()=>{
+    const name="sivaprasad";
+    const age=27;
+    const mobile=9346564401;
+    const place="markapur";
+    const employe_id=11413;
+    return name+ " is " + age + " years old" + mobile +"contact me" + place + "is my native place" + employe_id + "of sightspectrum"
+};
+export default message;
